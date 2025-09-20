0618d7ddd9b5894557bc89bc6895909139f16d62
Oleg Fomin <ofstudio@gmail.com>
2025-09-20 12:17:48 +0300

    update config defaults

--- internal/config/config.go
+++ internal/config/config.go
@@ -27,25 +27,25 @@ type Telegram struct {
 
 // Settings - application settings
 type Settings struct {
-	PublicUrl       url.URL                 `env:"PUBLIC_URL,required"`      // Public URL where the public directory is accessible (used in feed)
-	PublicDir       string                  `env:"PUBLIC_DIR,required"`      // Path to public directory where feed and media files are stored
-	DownloadDir     string                  `env:"DOWNLOAD_DIR,required"`    // Path to temporary download directory
-	DownloadTimeout time.Duration           `env:"DOWNLOAD_TIMEOUT"`         // Timeout for downloading media
-	DownloadFormat  entities.DownloadFormat `env:"DOWNLOAD_FORMAT"`          // Media download format by default (e.g., mp3, m4a)
-	DownloadQuality string                  `env:"DOWNLOAD_QUALITY"`         // Media download quality (e.g., 192k)
-	ThumbnailSize   int                     `env:"THUMBNAIL_SIZE"`           // Size of the square thumbnail to generate (in pixels)
-	YtDlpPath       string                  `env:"YT_DLP_PATH"`              // Path to yt-dlp executable
-	FFMpegPath      string                  `env:"FFMPEG_PATH"`              // Path to ffmpeg executable
-	FeedFileName    string                  `env:"FEED_FILENAME"`            // Name of the RSS feed file (will be created in PublicDir)
-	FeedTitle       string                  `env:"FEED_TITLE"`               // Title of the RSS feed
-	FeedDescription string                  `env:"FEED_DESC"`                // Description of the RSS feed
-	FeedImage       string                  `env:"FEED_IMAGE,required"`      // URL of the RSS feed cover image
-	FeedLanguage    string                  `env:"FEED_LANGUAGE,required"`   // Language of the RSS feed (e.g., en)
-	FeedCategories  []string                `env:"FEED_CATEGORIES,required"` // Categories of the RSS feed
-	FeedCategories2 []string                `env:"FEED_CATEGORIES2"`         // Additional categories of the RSS feed
-	FeedCategories3 []string                `env:"FEED_CATEGORIES3"`         // Additional categories of the RSS feed
-	FeedIsExplicit  bool                    `env:"FEED_IS_EXPLICIT"`         // Whether the feed contains explicit content
-	FeedAuthor      string                  `env:"FEED_AUTHOR"`              // Author of the RSS feed
-	FeedLink        string                  `env:"FEED_LINK"`                // Link to the website of the RSS feed
-	FeedKeywords    string                  `env:"FEED_KEYWORDS"`            // Comma-separated keywords for the RSS feed
+	PublicUrl       url.URL                 `env:"PUBLIC_URL,required"`   // Public URL where the public directory is accessible (used in feed)
+	PublicDir       string                  `env:"PUBLIC_DIR,required"`   // Path to public directory where feed and media files are stored
+	DownloadDir     string                  `env:"DOWNLOAD_DIR,required"` // Path to temporary download directory
+	DownloadTimeout time.Duration           `env:"DOWNLOAD_TIMEOUT"`      // Timeout for downloading media
+	DownloadFormat  entities.DownloadFormat `env:"DOWNLOAD_FORMAT"`       // Media download format by default (e.g., mp3, m4a)
+	DownloadQuality string                  `env:"DOWNLOAD_QUALITY"`      // Media download quality (e.g., 192k)
+	ThumbnailSize   int                     `env:"THUMBNAIL_SIZE"`        // Size of the square thumbnail to generate (in pixels)
+	YtDlpPath       string                  `env:"YT_DLP_PATH"`           // Path to yt-dlp executable
+	FFMpegPath      string                  `env:"FFMPEG_PATH"`           // Path to ffmpeg executable
+	FeedFileName    string                  `env:"FEED_FILENAME"`         // Name of the RSS feed file (will be created in PublicDir)
+	FeedTitle       string                  `env:"FEED_TITLE"`            // Title of the RSS feed
+	FeedDescription string                  `env:"FEED_DESC"`             // Description of the RSS feed
+	FeedImage       string                  `env:"FEED_IMAGE"`            // URL of the RSS feed cover image
+	FeedLanguage    string                  `env:"FEED_LANGUAGE"`         // Language of the RSS feed (e.g., en)
+	FeedCategories  []string                `env:"FEED_CATEGORIES"`       // Categories of the RSS feed
+	FeedCategories2 []string                `env:"FEED_CATEGORIES2"`      // Additional categories of the RSS feed
+	FeedCategories3 []string                `env:"FEED_CATEGORIES3"`      // Additional categories of the RSS feed
+	FeedIsExplicit  bool                    `env:"FEED_IS_EXPLICIT"`      // Whether the feed contains explicit content
+	FeedAuthor      string                  `env:"FEED_AUTHOR"`           // Author of the RSS feed
+	FeedLink        string                  `env:"FEED_LINK"`             // Link to the website of the RSS feed
+	FeedKeywords    string                  `env:"FEED_KEYWORDS"`         // Comma-separated keywords for the RSS feed
 }
