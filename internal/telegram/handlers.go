026c88fa2e4e26b455c91865b3aa82fd92ea7671
Oleg Fomin <ofstudio@gmail.com>
2025-09-20 18:56:47 +0300

    refactor message handling and send request in safe way

--- internal/telegram/handlers.go
+++ internal/telegram/handlers.go
@@ -2,7 +2,10 @@ package telegram
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"log/slog"
+	"time"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
@@ -17,6 +20,10 @@ type Handlers struct {
 	builder   Builder
 	settings  config.Settings
 }
+
+var (
+	errProcessorBusy = errors.New("processor is busy")
+)
 
 func NewHandlers(settings config.Settings, log *slog.Logger, processor Processor, builder Builder) *Handlers {
 	return &Handlers{
@@ -38,20 +45,8 @@ func (h *Handlers) CmdStart() bot.HandlerFunc {
 		if update.Message == nil {
 			return
 		}
-
 		h.log.Info("[bot] start command received", "update_id", update.ID, "message", logMessage(update.Message))
-
-		_, err := b.SendMessage(ctx, &bot.SendMessageParams{
-			ChatID: update.Message.Chat.ID,
-			Text:   locales.MsgStart,
-		})
-
-		if err != nil {
-			h.log.Error("[bot] failed to send start message",
-				"error", err.Error(), "chat", logChat(&update.Message.Chat))
-		} else {
-			h.log.Info("[bot] start message sent", "chat", logChat(&update.Message.Chat))
-		}
+		h.sendMessage(ctx, b, update.Message.Chat, locales.MsgStart)
 	}
 }
 
@@ -66,20 +61,11 @@ func (h *Handlers) CmdBuild() bot.HandlerFunc {
 		if err != nil {
 			h.log.Error("[bot] failed to build podcast feed",
 				"error", err.Error(), "chat", logChat(&update.Message.Chat))
-			_, _ = b.SendMessage(ctx, &bot.SendMessageParams{
-				ChatID: update.Message.Chat.ID,
-				Text:   locales.MsgBuildError,
-			})
+			h.sendMessage(ctx, b, update.Message.Chat, locales.MsgBuildError)
 			return
 		}
 
-		if _, err = b.SendMessage(ctx, &bot.SendMessageParams{
-			ChatID: update.Message.Chat.ID,
-			Text:   locales.MsgBuildSuccess,
-		}); err != nil {
-			h.log.Error("[bot] failed to send build success message",
-				"error", err.Error(), "chat", logChat(&update.Message.Chat))
-		}
+		h.sendMessage(ctx, b, update.Message.Chat, locales.MsgBuildSuccess)
 	}
 }
 
@@ -97,19 +83,52 @@ func (h *Handlers) Url() bot.HandlerFunc {
 			Force:     false,
 		}
 
-		select {
-		case h.processor.In() <- request:
-		default:
-			// Processor is busy
-			h.log.Info("[bot] processor is busy, request dropped", "request", request.LogValue())
-			// Send "busy" message
-			if _, err := b.SendMessage(ctx, &bot.SendMessageParams{
-				ChatID: update.Message.Chat.ID,
-				Text:   locales.MsgDownloadBusy,
-			}); err != nil {
-				h.log.Error("[bot] failed to send busy message",
-					"error", err.Error(), "chat", logChat(&update.Message.Chat))
-			}
+		msg := locales.MsgDownloadStarted
+		err := h.sendRequest(ctx, request)
+		// if the processor is busy
+		if errors.Is(err, errProcessorBusy) {
+			msg = locales.MsgDownloadBusy
+			h.log.Error("[bot] processor is busy, request dropped",
+				"request", request.LogValue())
+
 		}
+		// if there was another error
+		if err != nil {
+			msg = locales.MsgSomethingWentWrong
+			h.log.Error("[bot] failed to queue request",
+				"error", err.Error(), "request", request.LogValue())
+		}
+
+		// send response message
+		h.sendMessage(ctx, b, update.Message.Chat, msg)
+
 	}
+}
+
+// sendRequest tries to send the request to the processor safely.
+func (h *Handlers) sendRequest(ctx context.Context, req *entities.Request) error {
+	select {
+	// if the request was successfully sent to the processor
+	case h.processor.In() <- req:
+		return nil
+	// if the processor is busy (no worker available in 2 seconds)
+	case <-time.After(time.Second * 2):
+		return errProcessorBusy
+	// if the context is done (app shutting down)
+	case <-ctx.Done():
+		return fmt.Errorf("context done: %w", ctx.Err())
+	}
+}
+
+// sendMessage sends text message to the specified chat.
+func (h *Handlers) sendMessage(ctx context.Context, b *bot.Bot, c models.Chat, t string) {
+	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:    c.ID,
+		Text:      t,
+		ParseMode: "MarkdownV2",
+	})
+	if err != nil {
+		h.log.Error("[bot] failed to send message", "error", err.Error(), "chat", logChat(&c))
+	}
+	h.log.Info("[bot] message sent", "chat", logChat(&c))
 }
